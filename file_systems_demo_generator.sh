e6d82632378eec9784daf10e1c8fe80ea1e7317d
Lukas Tobler <luk4s.tobler@gmail.com>
2017-08-25 13:11:32 +0200

    Improve file_systems_demo_generator.sh

--- file_systems_demo_generator.sh
+++ file_systems_demo_generator.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+pushd .
 cd ./HackingSessionExercises/fileSystems/
 fallocate -l 16M TestDisk
 fallocate -l 1M partition1
 fallocate -l 1M partition2
 fallocate -l 1M image.iso
 echo "You successfully wrote an image to fake disk!" > image.iso
+popd
