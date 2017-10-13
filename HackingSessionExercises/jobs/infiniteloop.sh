bf8d392960e322831cde614a13d40ca0780dc5ed
Lukas Tobler <luk4s.tobler@gmail.com>
2017-10-13 14:47:24 +0200

    Make infiniteloop.sh POSIX compatible

--- HackingSessionExercises/jobs/infiniteloop.sh
+++ HackingSessionExercises/jobs/infiniteloop.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-while :
-	do
-		echo "this is an infinite loop... kill me if you can"
-		sleep 1
-	done
+while : do
+    echo "this is an infinite loop... kill me if you can"
+    sleep 1
+done
