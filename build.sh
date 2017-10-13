5f0ee02a3929bfc9da028c67113cb463af791a1b
Nils <nilsl@student.ethz.ch>
2017-10-13 19:42:16 +0200

    Use long version of flags

--- build.sh
+++ build.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
-pandoc_flags='-H layout/header.tex --template=layout/layout.tex --listings --latex-engine=xelatex'
+pandoc_flags='--include-in-header=layout/header.tex --template=layout/layout.tex --listings --latex-engine=xelatex'
 
 # Generate git revision information.
 # Will be included in the document header.
 rev=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 echo "$rev" > git_tag
 
-# Build PDFs
+# Build exercises pdf
 echo "Building: exercises"
-pandoc $(ls [0-9]*-*.md) $pandoc_flags -o exercises.pdf
-[[ $? == 0 ]] && echo "ok"
-echo "Building: solutions"
-pandoc $(ls solutions/[0-9]*-*.md) $pandoc_flags -o solutions.pdf
-[[ $? == 0 ]] && echo "ok"
+pandoc $(ls [0-9]*-*.md) $pandoc_flags --output exercises.pdf
+if [[ $? == 0 ]]; then
+	echo "ok"
+else
+	echo "building exercises failed with status $?"
+fi
 
+# Build solutions pdf
+echo "Building: solutions"
+pandoc $(ls solutions/[0-9]*-*.md) $pandoc_flags --output solutions.pdf
+if [[ $? == 0 ]]; then
+	echo "ok"
+else
+	echo "building solutions failed with status $?"
+fi
