ce9121f05435c3ba936001a6948b4b8a989c3ddd
Nils <nilsl@student.ethz.ch>
2017-10-15 04:15:14 +0200

    build.sh: Add possibility to compile to tex easily

--- build.sh
+++ build.sh
@@ -12,9 +12,15 @@ pandoc_flags="--include-in-header=layout/header.tex --template=layout/layout.tex
 rev=$(printf "r%s.%s" "$(git rev-list --count HEAD)" "$(git rev-parse --short HEAD)")
 echo "$rev" > git_tag
 
+# Provide option to output tex file. Looking at the tex file makes debugging layout much easier.
+out_format="pdf"
+if [[ $1 == "--tex" ]]; then
+	out_format="AUTOGEN.tex"
+fi
+
 # Build exercises pdf
 echo "Building: exercises"
-pandoc $(ls [0-9]*-*.md) $pandoc_flags --output exercises.pdf
+pandoc $(ls -- [0-9]*-*.md) $pandoc_flags --output "exercises.$out_format"
 if [[ $? == 0 ]]; then
 	echo "ok"
 else
@@ -23,7 +29,7 @@ fi
 
 # Build solutions pdf
 echo "Building: solutions"
-pandoc $(ls solutions/[0-9]*-*.md) $pandoc_flags --output solutions.pdf
+pandoc $(ls -- solutions/[0-9]*-*.md) $pandoc_flags --output "solutions.$out_format"
 if [[ $? == 0 ]]; then
 	echo "ok"
 else
